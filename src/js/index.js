a9072f1a13e43434a07cdce6e4fd33ced95090cf
amitesh92 <amiteshmaurya01@gmail.com>
2017-02-21 20:43:31 +0530

    changed by amitesh

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
     $('.carousel').carousel({
         interval: 3000
-    })
+    });
+
+
+
+    if ($(window).width() < 992) {
+        $('#menu').click(function() {
+
+            $('.menu-container').toggleClass('active');
+
+        });
+        $('.navigation li').click(function() {
+
+            $(this).find(' ul').slideToggle(500);
+        });
+    }
+
+
 });
